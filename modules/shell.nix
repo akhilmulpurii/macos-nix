a7a87325ac481a188429e22591397f308eea59cf
Akhil Mulpuri <akhil@Valhalla.local>
2026-09-03 01:10:28 -0500

    Configure Fish as the default shell
    Add a Darwin rebuild alias and register Fish as an available and user
    default shell.

--- modules/shell.nix
+++ modules/shell.nix
@@ -1,4 +1,11 @@
 { pkgs, ... }:
 {
-  programs.fish.enable = true;
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      rebuild = "sudo darwin-rebuild switch --flake /etc/nix-darwin#Valhalla";
+    };
+  };
+  environment.shells = [ pkgs.fish ];
+  users.users.akhil.shell = pkgs.fish;
 }
